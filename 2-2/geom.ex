234d28a1de338b4ea67bf036d1d442676f7a8178
lubien <lubien1996@gmail.com>
2016-07-30 06:47:00 -0300

    SAVEPOINT

---
+++ 2-2/geom.ex
@@ -0,0 +1,5 @@
+defmodule Geom do
+  def area(height \\ 1, width \\ 1) do
+    height * width
+  end
+end
---
+++ 2-3/geom.ex
@@ -0,0 +1,18 @@
+defmodule Geom do
+  @moduledoc """
+  Provides geometrial helpers
+
+  ## Examples
+
+    iex> Geom.area 2, 3
+    6
+
+  """
+
+  """
+  Calculates the area based on a height and a width
+  """
+  def area(height \\ 1, width \\ 1) do
+    height * width
+  end
+end
